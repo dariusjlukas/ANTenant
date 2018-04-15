44d55eff61ffa2a195f6c34d4d6d6d7a73efa919
Darius Lukas <darius@thanics.com>
2018-04-14 20:45:56 -0400

    added a recording example and updated the compile script

--- piCompile.sh
+++ piCompile.sh
@@ -6,5 +6,6 @@ echo "compiling..."
   g++ -c -g `pkg-config --cflags alsa` -o main.o ALSAtest.cpp
   g++ -o main main.o -lasound
   chmod +x main.o
+  gcc -o alsa-record-example -lasound alsa-record-example.c && ./alsa-record-example hw:0
   echo "finished" 
   
